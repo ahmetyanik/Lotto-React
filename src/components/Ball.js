b089def24891804538826183cd69b9c98cd03426
ahmetyanik <ahmetyank4242@gmail.com>
2021-12-05 21:52:06 +0100

    first commit

---
+++ src/components/Ball.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Ball({number,ballNumber}) {
+
+    return (
+        <div style={{width:"60px",height:"60px"}} className={`d-flex justify-content-center align-items-center border border-info rounded-circle mx-2 ${ballNumber===6 ? "bg-info" : null}`}>
+
+        {number}
+        </div>
+    )
+}
+
+export default Ball
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Balls from './Balls'
+import Header from './Header'
+
+function Main() {
+    return (
+        <div style={{minHeight:"100vh"}} className="container d-flex justify-content-center align-items-center flex-column">
+
+        <Header/>
+        <Balls/>
+            
+        </div>
+    )
+}
+
+export default Main
